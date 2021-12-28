6dc406a82332a2edb0fead947fe342511ec96db0
Franklin Obidike <Franklin@Obi-Franklin.local>
2021-12-28 13:32:12 +0100

    retrieved transaction reference from paystack

--- UtilityService/Helpers.js
+++ UtilityService/Helpers.js
@@ -2,7 +2,7 @@ import {toast} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 toast.configure()
 export const formatMoney = money => {
-  return new Intl.NumberFormat('de-DE', {
+  return new Intl.NumberFormat('en-NG', {
     style: 'currency',
     currency: 'NGN',
   }).format(money)
